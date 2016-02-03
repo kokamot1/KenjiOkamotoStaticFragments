07d0b0e4b90239c7240a09505c2714691fb77f1a
Kenji Okamoto <kokamot1@my.westga.edu>
2016-02-03 14:27:04 -0800

    Modify MainActivity to communicate with the fragments

--- app/src/main/java/edu/westga/kenjiokamotostaticfragments/MainActivity.java
+++ app/src/main/java/edu/westga/kenjiokamotostaticfragments/MainActivity.java
@@ -9,7 +9,18 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DataEntryFragment.DataEntryListener {
+
+    @Override
+    public void onDataEntry(double number1, double number2) {
+        DataDisplayFragment displayFragment =
+            (DataDisplayFragment) getSupportFragmentManager().findFragmentById(R.id.dataDisplayFragment);
+        displayFragment.setNumber1(number1);
+        displayFragment.setNumber2(number2);
+        displayFragment.multiply();
+        displayFragment.displayProduct();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
